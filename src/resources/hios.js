ed0b5937c392a5a84fcbbfcdbab96f3c8b40f921
Bill Barnes <Bill@Bills-MacBook-Pro-2.local>
2017-11-08 17:52:52 -0800

    add query table code

--- src/resources/hios.js
+++ src/resources/hios.js
@@ -11,6 +11,11 @@ const {
 const docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' })
 
 /* eslint-disable no-nested-ternary */
-export const getApplicationTemplate = hios => {
-
+export const getApplicationTemplate = async (hios) => {
+  const template = await docClient.query({
+    TableName: 'prod-carrier-application-hios',
+    IndexName: 'Hios-index',
+    KeyConditionExpression: 'Hios = :hios',
+    ExpressionAttributeValues: { ':hios': hios },
+  }).promise();
 };
