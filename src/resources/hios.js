310eb7c94c086b4f19d2982e2e56242e92901134
Bill Barnes <Bill@Bills-MacBook-Pro-2.local>
2017-11-08 17:53:21 -0800

    refactor

--- src/resources/hios.js
+++ src/resources/hios.js
@@ -1,12 +1,4 @@
-import { isObject } from 'lodash'
 import AWS from 'aws-sdk'
-import moment from 'moment'
-
-const {
-  STAGE: stage,
-  EFFECTIVE_DATE = '20180101',
-  HEALTH_BUNDLE = 'HealthBundle',
-} = process.env
 
 const docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' })
 
