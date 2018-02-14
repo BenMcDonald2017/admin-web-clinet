72c803682a00299da7610fceff6ed5d54c96d374
Greg Cardoni <greg@cardoni.net>
2018-02-13 16:09:52 -0800

    revert to previous, known, working build process for right this second

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,37 @@
-/* eslint-disable import/no-extraneous-dependencies, no-console */
+// eslint-disable-next-line
+const webpack = require('webpack');
+
 module.exports = {
   devtool: 'source-map',
-  externals: [/aws-sdk/, /electron/],
+  externals: [
+    /aws-sdk/,
+    /electron/,
+  ],
+  target: 'node',
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader?cacheDirectory=true',
-        options: {
-          presets: ['@babel/preset-env'],
-          plugins: [
-            require('@babel/plugin-proposal-object-rest-spread'),
-            require('@babel/plugin-proposal-optional-chaining'),
-            require('@babel/plugin-transform-runtime'),
-          ],
-        },
+    loaders: [{
+      test: /\.js$/i,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        cacheDirectory: true,
+        presets: [
+          'es2017',
+          'es2016',
+          'es2015-node6',
+        ],
+        plugins: [
+          'add-module-exports',
+          'transform-class-properties',
+          'transform-export-extensions',
+          'transform-object-rest-spread',
+        ],
       },
+      rules: [{
+        test: /\.js$/i,
+        use: 'source-map-loader',
+        enforce: 'pre',
+      }],
     }],
   },
 }
